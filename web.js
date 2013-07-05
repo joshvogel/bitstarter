f132b87d6a3fc8ce6fa70e082938454968c9d9f9
Josh Vogel <joshvogel17@gmail.com>
2013-07-05 18:56:05 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buffer = fs.readFileSync(index.html);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
